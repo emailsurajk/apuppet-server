3c99aa7a106d32b91cc7caf10894c325e6740954
suraj <email.suraj.k@gmail.com>
2026-04-24 22:53:31 +0530

    update

--- web-admin/js/remote-video.js
+++ web-admin/js/remote-video.js
@@ -52,6 +52,7 @@ function RemoteVideo(remoteVideoElem, videoLoader, videoStats) {
         this.remoteVideoElem.css('transform', 'rotate(' + normalized + 'deg)');
         this.remoteVideoElem.css('transform-origin', 'center center');
         $('#deviceGestures').attr('data-rotation', normalized);
+        $('#btnRotateVideo').text('Rotate View (' + normalized + '°)');
         this._applyLayout();
     }
 
